c85f34c4466c4916e97dc4a72d602ed2563b1c8d
LucasHBG <lukahbg@gmail.com>
2021-09-02 13:34:58 -0300

    initial setup

---
+++ src/main/java/com/employee/service/interaction/esinteraction/EsInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.employee.service.interaction.esinteraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsInteractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/service/interaction/esinteraction/EsInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.service.interaction.esinteraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
