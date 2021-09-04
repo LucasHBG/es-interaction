8f17731302823b4ce017589c63a522c8d8ad114e
LucasHBG <lukahbg@gmail.com>
2021-09-04 13:12:48 -0300

    Refactoring file main

--- src/main/java/com/employee/service/interaction/esinteraction/EsInteractionApplication.java
+++ src/main/java/com/employee/service/interaction/esinteraction/EsInteractionApplication.java
@@ -3,6 +3,9 @@ package com.employee.service.interaction.esinteraction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author LucasHBG
+ */
 @SpringBootApplication
 public class EsInteractionApplication {
 
