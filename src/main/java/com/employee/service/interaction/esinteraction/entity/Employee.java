a1be60ad2c25d903c0eb645ce00038f9cdaa044e
LucasHBG <lukahbg@gmail.com>
2021-09-23 15:20:07 -0300

    add serializable for entity and @Temporal also set updatable and insertable booleans as false on @Column

--- src/main/java/com/employee/service/interaction/esinteraction/entity/Employee.java
+++ src/main/java/com/employee/service/interaction/esinteraction/entity/Employee.java
@@ -1,12 +1,16 @@
 package com.employee.service.interaction.esinteraction.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.employee.service.interaction.esinteraction.utils.JsonDateSerializer;
 import com.employee.service.interaction.esinteraction.utils.JsonDatetimeSerializer;
@@ -15,28 +19,41 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 /**
  * @author LucasHBG
+ * A anotação @Entity avisa o Hibernate pra criar uma tabela dessa classe(usado em bancos embedded, como H2)
+ * Serializable para transformar o objeto em um vetor de bytes para facilitar a comunicação, persistencia dos dados em
+ * um banco de dados e a deserialização.
  */
 @Entity
 @Table(name = "employee")
-public class Employee {
+public class Employee implements Serializable {
 
     /**
      * Anotação do spring para variáveis que se auto incrementam pela entidade de persistencia.
-     * Deixar o Hibernate incrementar resulta numa melhor performance
+     * Deixar o Hibernate incrementar resulta numa melhor performance devido os mecanismos próprios de otimização
      */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_num")
     private Long    employeeNum;
     
     private String  firstName;
 
+    @Column(nullable = true)
     private Date    birthDate;
 
+    /**
+     * Essa coluna é ignorada ao tentar fazer update ou inserts, pois é um valor que não precisa mudar
+     */
+    @Column(name = "created_datetime", updatable = false, insertable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date    createdDatetime;
 
-    public Employee() {
-        
-    }
+    /** 
+     *    Construtor vazio para o JPA usar. Não se deve utilizá-lo diretamente.
+     *    Assim como proposto na documentação do Spring Boot
+     *    Fonte: https://spring.io/guides/gs/accessing-data-jpa/
+     */ 
+    protected Employee() {}
 
     public Employee(Long employeeNum, String firstName, Date birthDate, Date createdDatetime) {
         this.employeeNum        = employeeNum;
@@ -54,6 +71,7 @@ public class Employee {
         this.employeeNum = employeeNum;
     }
 
+    @Column(nullable = false)
     public String getFirstName() {
         return firstName;
     }
