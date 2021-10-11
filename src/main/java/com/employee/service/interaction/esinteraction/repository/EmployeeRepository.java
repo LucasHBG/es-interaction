5dfc5b5e944a8c156ee215bdfffcfd7ef22b82be
LucasHBG <lukahbg@gmail.com>
2021-10-11 13:51:57 -0300

    added findByFirstNameStartingWith

--- src/main/java/com/employee/service/interaction/esinteraction/repository/EmployeeRepository.java
+++ src/main/java/com/employee/service/interaction/esinteraction/repository/EmployeeRepository.java
@@ -1,12 +1,17 @@
 package com.employee.service.interaction.esinteraction.repository;
 
+import java.util.List;
+
 import com.employee.service.interaction.esinteraction.entity.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- *  Escolhi JPA no lugar do CRUD devido o Jpa retornar List<> no lugar de Iterable<S>
+ * Escolhi JPA no lugar do CRUD devido o Jpa retornar List<> no lugar de
+ * Iterable<S>
  */
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-    
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByFirstNameStartingWith(String name);
+
 }
