ac0223fc2c59f1a687749b4776695a3024df898c
LucasHBG <lukahbg@gmail.com>
2022-03-09 11:00:26 -0300

    feat: added comments to class

--- src/main/java/com/employee/service/interaction/esinteraction/utils/JsonDateSerializer.java
+++ src/main/java/com/employee/service/interaction/esinteraction/utils/JsonDateSerializer.java
@@ -9,8 +9,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+/**
+ * Classe criada para formatar a data padrão do servidor backend 
+ * ao receber solicitações  
+ */
 public class JsonDateSerializer extends JsonSerializer<Date> {
 
+    //Trocando o tipo de data usada pelo sistema para dia/mes/ano
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
